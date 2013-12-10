de6f3bebd058c8e42601a8a863b3e33f37711569
Phaneendra Mettu <mettu.phaneendra@gmail.com>
2013-12-09 17:23:05 -0800

    Adding spring controller context xml file.

--- src/main/java/com/itag/services/register/controller/RegistrationController.java
+++ src/main/java/com/itag/services/register/controller/RegistrationController.java
@@ -1,12 +1,16 @@
 package com.itag.services.register.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
+@RequestMapping(value = "/itag-services")
 public class RegistrationController {  
     String message = "iTag server registration service !";  
   
-    @RequestMapping("/itag-services/register")  
+    @RequestMapping(value="/register", method = RequestMethod.GET)  
     public ModelAndView showMessage() {  
         System.out.println("from controller");  
         return new ModelAndView("register", "message", message);  
